9fcda6f75286c0048fb6bf334d61a24eea84b3bc
Rajaabpro <rajaab.pro@gmail.com>
2025-02-10 21:07:40 +0500

    add validation for birthdate input and improve age calculation output

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function calculate() {
-  const birthdate = document.getElementById('birthdate').value.
-  value;
-  const birthDate = new Date(birthdate);
-const today = new Date();
-let age = today.getFullYear() - birthDate.getFullYear();
-const monthDifference = today.getMonth() - birthDate.getMonth();
-if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
-  age--;
-
+    const birthdate = document.getElementById('birthdate').value;
+    if (!birthdate) {
+      alert("Please enter your birthdate!"); 
+      return;
     }
-document.getElementById('result').innerText = `your age is ${age} years old`;
-}
+  
+    const birthDate = new Date(birthdate);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+  
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+  
+    document.getElementById('result').innerText = `Your age is ${age} years old.`;
+  }
